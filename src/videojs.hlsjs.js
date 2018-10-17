2b38f968bc15e6fbdbf9a17efc6b0cafb69c8c43
Stephan Hesse <tchakabam@gmail.com>
2018-10-17 22:18:49 +0200

    convert to es6 idioms

--- src/videojs.hlsjs.js
+++ src/videojs.hlsjs.js
@@ -1,5 +1,4 @@
 import Hls from 'hls.js';
-
 import videojs from 'video.js'; // resolved UMD-wise through webpack
 
 (function (videojs) {
@@ -10,144 +9,150 @@ import videojs from 'video.js'; // resolved UMD-wise through webpack
    * @param tech
    * @constructor
    */
-  function Html5HlsJS(source, tech) {
-    var options = tech.options_;
-    var el = tech.el();
-    var duration = null;
-    var hls = this.hls = new Hls(options.hlsjsConfig);
+  class Html5HlsJS {
 
-    /**
-     * creates an error handler function
-     * @returns {Function}
-     */
-    function errorHandlerFactory() {
-      var _recoverDecodingErrorDate = null;
-      var _recoverAudioCodecErrorDate = null;
+    constructor(source, tech) {
 
-      return function() {
-        var now = Date.now();
+      var options = tech.options_;
+      var el = tech.el();
+      var hls = this.hls = new Hls(options.hlsjsConfig);
 
-        if (!_recoverDecodingErrorDate || (now - _recoverDecodingErrorDate) > 2000) {
-          _recoverDecodingErrorDate = now;
-          hls.recoverMediaError();
-        }
-        else if (!_recoverAudioCodecErrorDate || (now - _recoverAudioCodecErrorDate) > 2000) {
-          _recoverAudioCodecErrorDate = now;
-          hls.swapAudioCodec();
-          hls.recoverMediaError();
+      /**
+       * creates an error handler function
+       * @returns {Function}
+       */
+      function errorHandlerFactory() {
+        var _recoverDecodingErrorDate = null;
+        var _recoverAudioCodecErrorDate = null;
+
+        return function() {
+          var now = Date.now();
+
+          if (!_recoverDecodingErrorDate || (now - _recoverDecodingErrorDate) > 2000) {
+            _recoverDecodingErrorDate = now;
+            hls.recoverMediaError();
+          }
+          else if (!_recoverAudioCodecErrorDate || (now - _recoverAudioCodecErrorDate) > 2000) {
+            _recoverAudioCodecErrorDate = now;
+            hls.swapAudioCodec();
+            hls.recoverMediaError();
+          }
+          else {
+            console.error('Error loading media: File could not be played');
+          }
+        };
+      }
+
+      // create separate error handlers for hlsjs and the video tag
+      var hlsjsErrorHandler = errorHandlerFactory();
+      var videoTagErrorHandler = errorHandlerFactory();
+
+      // listen to error events coming from the video tag
+      el.addEventListener('error', function(e) {
+        var mediaError = e.currentTarget.error;
+
+        if (mediaError.code === mediaError.MEDIA_ERR_DECODE) {
+          videoTagErrorHandler();
         }
         else {
           console.error('Error loading media: File could not be played');
         }
-      };
-    }
+      });
 
-    // create separate error handlers for hlsjs and the video tag
-    var hlsjsErrorHandler = errorHandlerFactory();
-    var videoTagErrorHandler = errorHandlerFactory();
+      // update live status on level load
+      hls.on(Hls.Events.LEVEL_LOADED, function(event, data) {
+        this.duration = data.details.live ? Infinity : data.details.totalduration;
+      });
 
-    // listen to error events coming from the video tag
-    el.addEventListener('error', function(e) {
-      var mediaError = e.currentTarget.error;
-
-      if (mediaError.code === mediaError.MEDIA_ERR_DECODE) {
-        videoTagErrorHandler();
-      }
-      else {
-        console.error('Error loading media: File could not be played');
-      }
-    });
-
-    /**
-     * Destroys the Hls instance
-     */
-    this.dispose = function() {
-      hls.destroy();
-    };
-
-    /**
-     * returns the duration of the stream, or Infinity if live video
-     * @returns {Infinity|number}
-     */
-    this.duration = function() {
-      return duration || el.duration || 0;
-    };
-
-    // update live status on level load
-    hls.on(Hls.Events.LEVEL_LOADED, function(event, data) {
-      duration = data.details.live ? Infinity : data.details.totalduration;
-    });
-
-    // try to recover on fatal errors
-    hls.on(Hls.Events.ERROR, function(event, data) {
-      if (data.fatal) {
-        switch (data.type) {
-          case Hls.ErrorTypes.NETWORK_ERROR:
-            hls.startLoad();
-            break;
-          case Hls.ErrorTypes.MEDIA_ERROR:
-            hlsjsErrorHandler();
-            break;
-          default:
-            console.error('Error loading media: File could not be played');
-            break;
+      // try to recover on fatal errors
+      hls.on(Hls.Events.ERROR, function(event, data) {
+        if (data.fatal) {
+          switch (data.type) {
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              hls.startLoad();
+              break;
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              hlsjsErrorHandler();
+              break;
+            default:
+              console.error('Error loading media: File could not be played');
+              break;
+          }
         }
+      });
+
+      Object.keys(Hls.Events).forEach(function(key) {
+        var eventName = Hls.Events[key];
+        hls.on(eventName, function(event, data) {
+          tech.trigger(eventName, data);
+        });
+      });
+
+      // Intercept native TextTrack calls and route to video.js directly only
+      // if native text tracks are not supported on this browser.
+      if (!tech.featuresNativeTextTracks) {
+        Object.defineProperty(el, 'textTracks', {
+          value: tech.textTracks,
+          writable: false
+        });
+        el.addTextTrack = function() {
+          return tech.addTextTrack.apply(tech, arguments);
+        };
       }
-    });
 
-    Object.keys(Hls.Events).forEach(function(key) {
-      var eventName = Hls.Events[key];
-      hls.on(eventName, function(event, data) {
-        tech.trigger(eventName, data);
-      });
-    });
-
-    // Intercept native TextTrack calls and route to video.js directly only
-    // if native text tracks are not supported on this browser.
-    if (!tech.featuresNativeTextTracks) {
-      Object.defineProperty(el, 'textTracks', {
-        value: tech.textTracks,
-        writable: false
-      });
-      el.addTextTrack = function() {
-        return tech.addTextTrack.apply(tech, arguments);
-      };
+      // attach hlsjs to videotag
+      hls.attachMedia(el);
+      hls.loadSource(source.src);
     }
 
-    // attach hlsjs to videotag
-    hls.attachMedia(el);
-    hls.loadSource(source.src);
+    /**
+     * Returns duration of media
+     */
+    duration() {
+      return this.duration || el.duration || 0;
+    }
+
+    /**
+     * Dispose
+     */
+    dispose() {
+      this.hls.destroy();
+    }
+
   }
 
-  var hlsTypeRE = /^application\/(x-mpegURL|vnd\.apple\.mpegURL)$/i;
-  var hlsExtRE = /\.m3u8/i;
+  const HLS_MIME_TYPE_REGEX = /^application\/(x-mpegURL|vnd\.apple\.mpegURL)$/i;
+  const HLS_M3U8_EXT_REGEX = /\.m3u8/i;
 
-  var HlsSourceHandler = {
-    canHandleSource: function(source) {
+  class HlsSourceHandler {
+
+    static canHandleSource(source) {
       if (source.skipContribHlsJs) {
         return '';
       }
-      else if (hlsTypeRE.test(source.type)) {
+      else if (HLS_MIME_TYPE_REGEX.test(source.type)) {
         return 'probably';
       }
-      else if (hlsExtRE.test(source.src)) {
+      else if (HLS_M3U8_EXT_REGEX.test(source.src)) {
         return 'maybe';
       }
       else {
         return '';
       }
-    },
-    handleSource: function(source, tech) {
+    }
+
+    static handleSource (source, tech) {
       return new Html5HlsJS(source, tech);
-    },
-    canPlayType: function(type) {
-      if (hlsTypeRE.test(type)) {
+    }
+
+    static canPlayType (type) {
+      if (HLS_MIME_TYPE_REGEX.test(type)) {
         return 'probably';
       }
 
       return '';
     }
-  };
+  }
 
   if (!Hls.isSupported()) {
     return;
