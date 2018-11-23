d9b77743f6e2465f1eb5c454910dcf0eedef2022
Stephan Hesse <tchakabam@gmail.com>
2018-11-23 01:50:03 +0100

    add example app for checking module loader compat

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,18 @@ const ENTRY_PATH = 'src/hlsjs-videojs-plugin'
 function makeConfig(options = {}) {
 
   const libName = options.libName || LIB_NAME;
+  const entryPath = options.entryPath || ENTRY_PATH;
 
   const config = {
     mode: process.env.WEBPACK_MODE || 'development',
-    entry: __dirname + '/' + ENTRY_PATH,
+    entry: __dirname + '/' + entryPath,
     devtool: 'source-map',
     output: {
       path: __dirname + '/' + DIST_DIR,
       publicPath: '/' + DIST_DIR +  '/',
       filename: libName + '.min.js',
       library: libName,
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      globalObject: 'this'
+      libraryTarget: 'umd'
     },
     resolve: {
       extensions: [".ts", ".js"]
@@ -49,9 +48,13 @@ function makeConfig(options = {}) {
   return config;
 }
 
+const appConfig = makeConfig({libName: LIB_NAME + 'AppExample', entryPath: 'examples/app'});
+appConfig.externals = {};
+
 const multiConfigs = [
   makeConfig(),
-  makeConfig({useExternalHlsjs: true, libName: LIB_NAME + 'External'})
+  makeConfig({useExternalHlsjs: true, libName: LIB_NAME + 'External'}),
+  appConfig
 ];
 
 module.exports = multiConfigs;
